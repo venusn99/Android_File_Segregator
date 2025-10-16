c78c83f49e28ffc621c2feea1279dfc1fbf155f8
venusn99 <venusn99@gmail.com>
2025-10-16 11:24:15 +0530

    Provided the unattened_files workflow

--- action.py
+++ action.py
@@ -6,6 +6,7 @@ class segregate_android_files():
         self._dst_dir = values['dst_dir']
         self.action = values['action']
         self.subfolder_format_number = values['subfolder_format_number']
+        self.unattend_files = values['unattend_files']
     def get_input_values(self):
         print(f'The source directory is {self._src_dir}\n')
         print(f'The destination directory is {self._dst_dir}\n')
@@ -15,15 +16,16 @@ class segregate_android_files():
         path2 = Dextractor.FilePathCheck(self._dst_dir)
         if path1.path_exists_check() and path2.path_exists_check():
             # below will create nested directories where ever the files are present at source directory
-            nested_obj=Dextractor.FolderAction(src_dir=self._src_dir, dst_dir=self._dst_dir, action =self.action.lower() ,format=self.subfolder_format_number).create_nested_directory()
+            nested_obj=Dextractor.FolderAction(src_dir=self._src_dir, dst_dir=self._dst_dir, action =self.action.lower() ,format=self.subfolder_format_number, unattend_files=self.unattend_files).create_nested_directory()
 
 
 if __name__ == "__main__":
-    src_dir = input("Enter the source directory: ")
-    dst_dir = input("Enter the destination directory: ")
-    action = input("Enter the action (copy/move): ")
-    subfolder_format_number = int(input("Enter the subfolder format number: \n\t1: YYYY \n\t2: YYYY/MM \n\t3: YYYY/MM/DD \n"))
-    config = {'src_dir': src_dir, 'dst_dir': dst_dir, 'action': action, 'subfolder_format_number': subfolder_format_number}
+    src_dir : str = input("Enter the source directory: ")
+    dst_dir : str = input("Enter the destination directory: ")
+    action : str = input("Enter the action (copy/move): ")
+    unattend_files : bool = input("Handle the unattended files? (True/False): ")
+    subfolder_format_number : int = int(input("Enter the subfolder format number: \n\t1: YYYY \n\t2: YYYY/MM \n\t3: YYYY/MM/DD \n"))
+    config = {'src_dir': src_dir, 'dst_dir': dst_dir, 'action': action, 'subfolder_format_number': subfolder_format_number, 'unattend_files': unattend_files}
 
     workflow1 = segregate_android_files(**config)
     workflow1.workflow()
